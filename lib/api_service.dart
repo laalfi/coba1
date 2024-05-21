cbac510c2272b7a2787db4ebc19babb1d4a231dd
laalfi <lailyalfina41@gmail.com>
2024-05-21 20:22:10 +0700

    first commit

---
+++ lib/api_service.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:coba/course_category_model.dart';
+
+class ApiService {
+  Future<List<CourseCategory>> fetchCourseCategories() async {
+    final response = await http
+        .get(Uri.parse('http://zonainformatika.com/api/coursecategories'));
+    if (response.statusCode == 200) {
+      final List<dynamic> jsonData = json.decode(response.body)['data'];
+      return jsonData.map((json) => CourseCategory.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to load course categories');
+    }
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:coba/api_service.dart';
+import 'package:coba/course_category_widget.dart';
+import 'package:coba/course_category_model.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late Future<List<CourseCategory>> futureCategories;
+
+  @override
+  void initState() {
+    super.initState();
+    futureCategories = ApiService().fetchCourseCategories();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Course Categories'),
+      ),
+      body: FutureBuilder<List<CourseCategory>>(
+        future: futureCategories,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                return CourseCategoryWidget(category: snapshot.data![index]);
+              },
+            );
+          }
+        },
+      ),
+    );
+  }
+}
